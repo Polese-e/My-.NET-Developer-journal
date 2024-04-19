05126d454f04c7fea4541cfef632d3c88a965c11
Polese-e <enzopolesedamasceno@gmail.com>
2024-04-18 23:54:55 -0300

    Assignment Operators

--- TiposDeDados/Program.cs
+++ TiposDeDados/Program.cs
@@ -1,14 +1,48 @@
 ﻿using ExemploFundamentos.Models;
 
-DateTime dataAtual = DateTime.Now.AddMonths(5);
-Console.WriteLine(dataAtual.ToString("dd/MM/yyyy HH/mm" ));
+// DateTime dataAtual = DateTime.Now.AddMonths(5);
+// Console.WriteLine(dataAtual.ToString("dd/MM/yyyy HH/mm" ));
 
-string apresentacao = "Olá Enzo";
+// string apresentacao = "Olá Enzo";
 
-Pessoa pessoa1 = new Pessoa();
+// Pessoa pessoa1 = new Pessoa();
 
-pessoa1.Nome = "Enzo";
-pessoa1.Idade = 19;
-pessoa1.Apresentar();
+// pessoa1.Nome = "Enzo";
+// pessoa1.Idade = 19;
+// pessoa1.Apresentar();
 
-Console.WriteLine(apresentacao);
+// Console.WriteLine(apresentacao);
+
+// ===================================================================================================================
+
+// int inteiro = 5;
+// string a = inteiro.ToString();
+
+// Console.WriteLine(a);
+
+// ===================================================================================================================
+
+// int a = 5;
+// double b = a;
+
+// Console.WriteLine(b);
+
+// int c  = 5;
+// long d =  c;
+
+// Console.WriteLine(d);
+
+// int e = int.MaxValue;
+// long f = e;
+
+// Console.WriteLine(f);
+
+// ===================================================================================================================
+
+// string a = "15-";
+
+// int b = 0;
+
+// int.TryParse(a, out b);
+
+// Console.WriteLine(b);
