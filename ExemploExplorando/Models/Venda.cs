69b751ec2d048548de1afcb8b7aa4fe63f75fe7e
Polese-e <enzopolesedamasceno@gmail.com>
2024-04-25 15:50:19 -0300

    Attributes in C#

--- ExemploExplorando/Models/Venda.cs
+++ ExemploExplorando/Models/Venda.cs
@@ -2,12 +2,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace ExemploExplorando.Models
 {
     public class Venda
     {
         public int Id { get; set; }
+        
+        [JsonProperty("Nome_Produto")]
         public string Produto { get; set; }
         public decimal Preco { get; set; }
         public DateTime DataVenda { get; set; }
--- ExemploExplorando/obj/Debug/net8.0/ExemploExplorando.AssemblyInfo.cs
+++ ExemploExplorando/obj/Debug/net8.0/ExemploExplorando.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b09f7322ebef394b70eaddfd3e68afde043ee14")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9b6648dae725b34af9f3b1c4ee6fbbd88c9e211")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
