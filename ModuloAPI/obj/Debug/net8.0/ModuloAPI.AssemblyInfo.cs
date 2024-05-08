2a0052025af6e34cf4dfad807348611538366efc
Polese-e <enzopolesedamasceno@gmail.com>
2024-05-08 18:40:24 -0300

    Update

--- ModuloAPI/obj/Debug/net8.0/ModuloAPI.AssemblyInfo.cs
+++ ModuloAPI/obj/Debug/net8.0/ModuloAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ModuloAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3cbc7ddf80743ef37833627bea3e51286fc0224")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2dfda1d7255ee6d166c94d14b9efbbc3b189d97e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ModuloAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ModuloAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
